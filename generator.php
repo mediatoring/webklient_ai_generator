a5ae2d5c147c6ad23f61a8d653cf7b9fd06c1370
mediatoring <104525162+mediatoring@users.noreply.github.com>
2024-07-28 22:15:07 +0200

    Update generator.php

--- generator.php
+++ generator.php
@@ -6,7 +6,7 @@
 * Version:           1.1
 * Author:            Webklient.cz & Kubicek.ai
 * Author URI:        https://www.webklient.cz
-* Text Domain:       Webklient-AI-article-generator-main
+* Text Domain:       webklient_ai_generator
 */
 
 if (!defined('ABSPATH')) {
